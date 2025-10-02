cb8a0b1fde9cf4a6eed61ed0b06173b715e35c5f
Oussama BITAOUI <oussamabitaa10@gmail.com>
2025-10-03 00:36:12 +0100

    update format

--- render-entrypoint.sh
+++ render-entrypoint.sh
@@ -1,20 +1,3 @@
-# #!/bin/sh
-
-# # Si la variable MYSQL_SSL_CA existe, créer le fichier /ca.pem
-# if [ -n "$MYSQL_SSL_CA" ]; then
-#     echo "📜 Writing SSL certificate to /ca.pem ..."
-#     echo "$MYSQL_SSL_CA" | base64 -d > /ca.pem
-#     chown www-data:www-data /ca.pem
-#     chmod 600 /ca.pem
-# else
-#     echo "⚠️ No SSL certificate found in MYSQL_SSL_CA"
-# fi
-
-# # Lancer Apache en premier plan
-# exec apache2-foreground
-
-
-
 #!/bin/sh
 
 # 1️⃣ Écrire le certificat SSL si fourni
